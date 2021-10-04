2d77ee94e5f4c425eef59c0f78dab01adcf410b0
Lorenzo Venerandi <68255980+Lore09@users.noreply.github.com>
2021-10-04 11:28:59 +0200

    Add files via upload

---
+++ Esercitazione 0/Produttore.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+// Produttore ORA e' un filtro
+public class Produttore {
+	public static void main(String[] args) {		
+		BufferedReader in = null;
+		
+		if (args.length != 1){
+			System.out.println("Utilizzo: produttore <inputFilename>");
+			System.exit(0);
+		}
+		
+		in = new BufferedReader(new InputStreamReader(System.in));
+			
+		FileWriter fout;
+		String inputl = null;
+		boolean status=true;
+
+		try {
+			fout = new FileWriter(args[0]);
+
+			while (status){
+				System.out.println("Inserisci la nuova riga");
+				inputl = in.readLine()+"\n";
+				if(input1.equals())
+				fout.write(inputl, 0, inputl.length());
+			}		
+			fout.close();
+		} 
+		catch (NumberFormatException nfe) { 
+			nfe.printStackTrace(); 
+			System.exit(1); // uscita con errore, intero positivo a livello di sistema Unix
+		}
+	    catch (IOException e) { 
+			e.printStackTrace();
+			System.exit(2); // uscita con errore, intero positivo a livello di sistema Unix
+		}
+	}
+}
+
