394445cbb4de60e9a6c1134c7a47bdda9f896cde
Lorenzo Venerandi <68255980+Lore09@users.noreply.github.com>
2021-11-19 15:54:41 +0100

    Shitpost di scheletri

---
+++ Esercitazione_6/RemOp/src/Risposta.java
@@ -0,0 +1,19 @@
+public class Risposta {
+
+	private String nomeFile;
+	private int nRighe;
+	public Risposta(String nomeFile, int nRighe) {
+		super();
+		this.nomeFile = nomeFile;
+		this.nRighe = nRighe;
+	}
+	@Override
+	public String toString() {
+		return "Risposta [nomeFile=" + nomeFile + ", nRighe=" + nRighe + "]";
+	}
+	
+	
+	
+	
+	
+}
---
+++ Esercitazione_6/RemOp/src/ServerImpl.java
@@ -0,0 +1,43 @@
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.Optional;
+
+public class ServerImpl extends UnicastRemoteObject implements RemOp {
+
+	protected ServerImpl() throws RemoteException {
+		super();
+	}
+
+	@Override
+	public int conta_righe(String fileName, int max) throws RemoteException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Optional<Risposta> elimina_riga(String fileName, int line) throws RemoteException {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+	
+	
+	public static void main(String[] args) {
+		final int REGISTRYPORT = 1099;
+		String registryHost = "localhost";
+		String serviceName = "RemOp"; // lookup name...
+
+		// Registrazione del servizio RMI
+		String completeName = "//" + registryHost + ":" + REGISTRYPORT + "/" + serviceName;
+		try {
+			ServerImpl serverRMI = new ServerImpl();
+			Naming.rebind(completeName, serverRMI);
+			System.out.println("Server RMI: Servizio \"" + serviceName + "\" registrato");
+		} catch (Exception e) {
+			System.err.println("Server RMI \"" + serviceName + "\": " + e.getMessage());
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+}
