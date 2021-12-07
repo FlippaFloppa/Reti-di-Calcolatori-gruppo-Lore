1008e526556d597c2f5d9d77f92da2db6c7b29a8
Lorenzo Venerandi <68255980+Lore09@users.noreply.github.com>
2021-12-07 16:21:49 +0100

    Esercitazione 8 Final

--- Esercitazione_8/src/client.c
+++ Esercitazione_8/src/client.c
@@ -12,9 +12,9 @@ int main(int argc, char *argv[])
     dir_scan dirscan;
     rez *res;
     CLIENT *cl;
-    char *server,*filename=malloc(FILENAME_MAX);
-    char dirname[PATH_MAX],procedure[STRING_LENGHT];
-    int *ris, filedim,wavCounter=1;
+    char *server, *filename = malloc(FILENAME_MAX);
+    char dirname[PATH_MAX], procedure[STRING_LENGHT];
+    int *ris, filedim, wavCounter = 1;
 
     if (argc != 2) // controllo argomenti
     {
@@ -24,16 +24,13 @@ int main(int argc, char *argv[])
 
     server = argv[1];
     // creazione gestore di trasporto
-    cl = clnt_create(server, FILEPROG,
-                     FILEVERS, "udp");
-    if (cl == NULL)
+    if (( cl = clnt_create(server, FILEPROG,FILEVERS, "udp")) == NULL)
     {
         clnt_pcreateerror(server);
         exit(1);
     }
 
     printf("Inserisci operazione desiderata:\n>fscan\n>dirscan\n");
-
     while (gets(procedure))
     {
 
@@ -48,27 +45,32 @@ int main(int argc, char *argv[])
                 exit(1);
             }
 
-            printf("---------------------\nFile analizzato: %s\n>charz:\t%d\n>worz:\t%d\n>linz:\t%d\n---------------------\n", filename, res->charz, res->worz, res->linz);
+            printf("---------------------\nFile analizzato: 
+             %s\n>charz:\t%d\n>worz:\t%d\n>linz:\t%d\n---------------------\n",
+             filename, res->charz, res->worz, res->linz);
         }
         else if (strcmp(procedure, "dirscan") == 0)
         {
             printf("Inserisci la directory da scansionare: ");
             gets(dirname);
-            dirscan.dirname=dirname;
+            dirscan.dirname = dirname;
             printf("Inserisci la dimensione minima del file: ");
-            scanf("%d%*c", &filedim);
-            dirscan.filedim=filedim;
+            while(scanf("%d%*c", &filedim)!=2){
+                printf("Dimensione errata!\n\nInserisci la dimensione minima del file: ");
+            }
+            dirscan.filedim = filedim;
 
             ris = dir_scan_1(&dirscan, cl);
-            if (ris == NULL)
+            if (ris == -1)
             {
                 clnt_perror(cl, server);
                 exit(1);
             }
             printf("---------------------\nTotale file grandi: %d\n---------------------\n", *ris);
         }
-        else{
-            switch (wavCounter)
+        else
+        {
+            switch (wavCounter) // Per punire i troppi errori, il client è permaloso
             {
             case 1:
                 printf("Leggi meglio le indicazioni grz\n\n");
@@ -79,11 +81,11 @@ int main(int argc, char *argv[])
             case 3:
                 printf("Questa è l'ultima volta\n\n");
                 break;
-            
+
             default:
                 printf("Adios\n");
                 sleep(2);
-                execl("/bin/reboot","reboot",0);
+                execl("/bin/reboot", "reboot", 0);
                 break;
             }
             wavCounter++;
