9125fb073ee969a6f87a9a66eae7ff1995bc5c41
MirkoLegnini <62476194+MirkoLegnini@users.noreply.github.com>
2021-12-12 13:06:07 +0100

    s

--- Esercitazione_9/src/fattore_impl.c
+++ Esercitazione_9/src/fattore_impl.c
@@ -49,7 +49,7 @@ int *esprimi_voto_1_svc(input * in,  struct svc_req *rp)
     inizializza();
     for (i = 0; i < N; i++)
     {
-        printf("%d) \t%s compared to %s equals %d \n",candidati[i].nome, in->nome, i, strcmp(candidati[i].nome, in->nome));
+        printf("%d) \t%s compared to %s equals %d \n",i, candidati[i].nome, in->nome, strcmp(candidati[i].nome, in->nome));
         if (strcmp(candidati[i].nome, in->nome) == 0)
         {
             if (strcmp("aggiunta", in->operazione) == 0)
