8aa7a7d50c346d88d0b8275002cd90daa1cf74d1
Lorenzo Venerandi <68255980+Lore09@users.noreply.github.com>
2021-12-13 16:20:23 +0100

    Pale

--- Esercitazione_8/src/implementazione.c
+++ Esercitazione_8/src/implementazione.c
@@ -12,29 +12,30 @@ rez *file_scan_1_svc(char **nomefile, struct svc_req *rp)
 {
     // Istanziazione risultato
     static rez res;
-    res.charz = 0;
-    res.worz = res.linz = 1;
+    res.charz = res.worz = res.linz = -1;
     int fd;
     printf("File:\t%s\n", *nomefile);
 
     //controllo esistenza del file
-    if ((fd = open(*nomefile, O_RDONLY)) == -1)
-        return NULL;
-
-    // Filtro a carattere
-    char c;
-    while (read(fd, &c, 1) > 0)
+    if (!(fd = open(*nomefile, O_RDONLY)) == -1)
     {
-        res.charz += 1;
-        if (c == ' ')
-            res.worz += 1;
-        if (c == '\n')
+        res.charz = 0;
+        res.worz = res.linz = 1;
+        // Filtro a carattere
+        char c;
+        while (read(fd, &c, 1) > 0)
         {
-            res.worz += 1;
-            res.linz += 1;
+            res.charz += 1;
+            if (c == ' ')
+                res.worz += 1;
+            if (c == '\n')
+            {
+                res.worz += 1;
+                res.linz += 1;
+            }
         }
+        close(fd);
     }
-    close(fd);
     return (&res);
 }
 
@@ -55,13 +56,12 @@ int *dir_scan_1_svc(dir_scan *req, struct svc_req *rp)
     }
 
     //main cicle
-    res=0;
+    res = 0;
     while ((dent = readdir(dir)) != NULL)
     {
         strcpy(path, req->dirname);
 
-        if (strncmp(dent->d_name, ".", 1) != 0 && dent->d_type==DT_REG 
-            &&(fd = open(strcat(strcat(path, "/"), dent->d_name), O_RDONLY)) != -1)
+        if (strncmp(dent->d_name, ".", 1) != 0 && dent->d_type == DT_REG && (fd = open(strcat(strcat(path, "/"), dent->d_name), O_RDONLY)) != -1)
         {
             if (lseek(fd, 0, SEEK_END) >= req->filedim)
                 res++;
